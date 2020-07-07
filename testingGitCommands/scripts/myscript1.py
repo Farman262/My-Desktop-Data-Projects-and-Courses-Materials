f038f87899073dbd8f74d3263b2f9b10376951da
Farman262 <farman2_00@yahoo.com>
2020-07-07 07:02:56 +0400

    Addig all my Desktop Data to my Remote Repository

---
+++ testingGitCommands/scripts/myscript1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import os
+import sys
+import math
+
+def check_reboot():
+	"""Return Ture if the Computer has a pending reboot """
+	return os.path.exist("/run/reboot-required"):
+	
+def main():
+	if check_reboot():
+		print("Pending Reboot.")
+		sys.exit(1)
+	print("Every thing is ok in this script")
+	sys.exit(0)
+def showName(name):
+	print("Your name is "+ name)
+	
+def fault():
+		print("The faulty function has been added")
+	
+	
+main()
+print("We import the os module")
+print("We import the os module")
+print("This is another Print Statement")
+print("This is another Print Statement")
+
+print("We import the os module and the line has been added to the print statement")
+print("We import the os module")
+showName("Farman Khan")
+faulty()
+faulter()
+
+
+
